55918357e223cf13c7d0a2c044e8abf3e58d045b
kWRizzz <krishnathey2j@gmail.com>
2026-04-19 22:50:30 +0530

    nav done

---
+++ backend/src/models/interviewReport.model.js
@@ -0,0 +1,51 @@
+const mongoose= require('mongoose')
+
+/**
+ * 
+ * Job Description Schema:String
+ * Resume :string
+ * Self Description:String
+ * 
+ * matchScore:Number
+ * 
+ * Technical Question:
+ * [
+ *  {
+ *      questions:""
+ *      intentions:""
+ *      Answers:""
+ *  }
+ * ]
+ * 
+ * Behaviour Questions:
+ * [
+ *  {
+ *      questions:""
+ *      intentions:""
+ *      Answers:""
+ *  }
+ * ]
+ * 
+ * Skill Gap:
+ * [
+ *  {
+ *      skills:""
+ *      sevirity:{
+ *                  type:String
+ *                  enum:["low","med","high"]   
+ *               }
+ *  }
+ * ]
+ * 
+ * prepration Plan:[{
+ *      
+ *      day:String
+ *      focus:String
+ *      task:[string] 
+ * }]
+ * 
+ **/ 
+
+
+
+
