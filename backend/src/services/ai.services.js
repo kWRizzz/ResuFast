226bd3ee682ebe2428e88463ef928ded9d5ad46a
kWRizzz <krishnathey2j@gmail.com>
2026-05-01 20:32:56 +0530

    Ai schema done

--- backend/src/services/ai.services.js
+++ backend/src/services/ai.services.js
@@ -1,30 +1,55 @@
 const { GoogleGenAI } = require('@google/genai')
 require('dotenv').config()
 
+const { z } = require('zod')
+const { zodToJsonSchema } = require('zod-to-json-schema')
+
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_API_KEY,
     backend: "google"
 })
-console.log(" env " + process.env.GOOGLE_API_KEY);
-const invockGemini = async () => {
-    try {
-        const res = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: "Hello this is my first time integrating ai in mern stack ab dekho kya kya gand phad cheze banti hai accha ye free cheez ki limit bhi hai ??  "
-        })
+// const invockGemini = async () => {
+//     try {
+//         const res = await ai.models.generateContent({
+//             model: "gemini-2.5-flash",
+//             contents: "Hello this is my first time integrating ai in mern stack ab dekho kya kya gand phad cheze banti hai accha ye free cheez ki limit bhi hai ??  "
+//         })
 
 
-        const text = res.candidates[0].content.parts[0].text;
+//         const text = res.candidates[0].content.parts[0].text;
 
-        console.log("AI Response:", text);
+//         console.log("AI Response:", text);
 
-    } catch (error) {
-        console.log("error in services ai is not working " + error.message);
+//     } catch (error) {
+//         console.log("error in services ai is not working " + error.message);
 
-    }
-}
+//     }
+// }
 
-module.exports = invockGemini
+const interviewReportSchema = z.object({
+    matchScore: z.number().description("A match score which varies from 0 to 100"),
+    technicalQuestions: z.array(z.object({
+        questions: z.string().description(" most important questions that can be asked "),
+        intentions: z.string().description("what was the intentions behind the questions asked "),
+        answers: z.string().description("what was the answers to that questions only in 100 words")
+    })).description("Tech Question should not exceed 100 words "),
+    BehaviourQuestions: z.array(z.object({
+        questions: z.string().description("most important questions that can be asked "),
+        intentions: z.string().description("what was the intentions behind the questions asked"),
+        answers: z.string().description("what was the answers to that questions only in 100 words")
+    })).description("Behaviour question should not exceed more than 100 words "),
+    skillGap: z.array(z.object({
+        skills: z.string().description("You lack this skills while you have these skills in 50 words max "),
+        severity: z.enum(["low", "medium", "high"]).description("severity in 50 words max")
+    })).description("skilgap should not exceed 60 words"),
+    preprationPlan:z.array(z.object({
+        days:z.number().description("No of days from today not exceed 15 words"),
+        focus:z.string().description("How much focus should you do not exceed 15 words"),
+        task:z.array(z.string()).description("what task to do not exceed more than 20 words")
+    })).description("preparation plan should not exceed 50 to 60 words")
+})
+
+module.exports
 
 
